4a50a43e01e43aa97a13de7c610f61c606731b4a
kaito hasegawa <kaitohasegawa000@gmail.com>
2020-01-13 19:07:38 +0900

    ルーティング設定

--- app/Http/Controllers/MainpageController.php
+++ app/Http/Controllers/MainpageController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Book;
 
 class MainpageController extends Controller
 {
     //
+    public function index(){
+        $books_all = Book::where("page", 1)->orderBy('book_id', 'desc');
+        $book = $books_all->paginate(10);
+        $params = [
+            'book' => $book
+        ];
+
+        return view('books.index', $params);
+    }
+
+    public function show($id){
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MainpageController@index');
+Route::resource('books', 'MainpageController');
